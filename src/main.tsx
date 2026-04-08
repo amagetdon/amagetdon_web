4d8acca8b16634dd84b23947a6f3e4caefd79df6
unknown <refra2n@naver.com>
2026-04-08 13:53:17 +0900

    첫 진입 시 스플래시 로딩 화면 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const splash = document.getElementById('splash')
+if (splash) {
+  splash.classList.add('hide')
+  setTimeout(() => splash.remove(), 400)
+}
+
 createRoot(document.getElementById('root')!).render(
   <App />,
 )
