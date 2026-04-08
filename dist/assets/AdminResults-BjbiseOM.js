c7468d1b581ec0ada3af35f91d7f3605d17d48ce
unknown <refra2n@naver.com>
2026-04-08 16:11:34 +0900

    dist 폴더 gitignore 추가

--- dist/assets/AdminResults-BjbiseOM.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as e}from"./index-BJyr8rWh.js";function a(){return t.jsx(e,{to:"/admin/site-settings",replace:!0})}export{a as default};
--- dist/assets/index-CqCa1FVf.js
+++
@@ -1,1 +0,0 @@
-import{y as r}from"./index-BJyr8rWh.js";var a=r();export{a as r};
